af6f664d826fa011ee88f334fcac863e59865d49
jackiegupta <longit.cqb@gmail.com>
2019-11-27 14:11:23 +0700

    cm

---
+++ src/main/java/project/shopfashion/ShopFashionApplication.java
@@ -0,0 +1,13 @@
+package project.shopfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFashionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFashionApplication.class, args);
+    }
+
+}
